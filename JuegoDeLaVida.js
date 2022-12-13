453e365187acb510f6b228c4bffd185efed40a56
FloresEsme <ing.esmeralda.of>
2022-12-12 18:04:14 -0600

    Crear la tabla utilizando una matriz desde un ciclo for anidado

---
+++ JuegoDeLaVida.js
@@ -0,0 +1,22 @@
+/*
+tabla()
+function tabla(){
+let filas = 5
+let colum = 5
+
+for (i = 0; i <filas; i++){
+    console.log(".")
+
+    for(j= 0; j<colum; j++){
+        console.log(".")
+    }
+    
+}
+console.log(tabla)
+*/
+
+for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+        console.log(i, ",", j)
+    }
+};
